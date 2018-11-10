49fd260527ec17816eb3622a233cb11ee1243376
SharingSmile <hapsoa@gmail.com>
2018-11-11 01:16:09 +0900

    complete radviz-example

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,13 @@
 <script>
 import Vue from 'vue';
 import * as d3 from 'd3';
-// import _ from 'lodash';
+import _ from 'lodash';
+
 import { store } from './vuex/store';
 
 Vue.prototype.$d3 = d3;
-// Vue.prototype.$_ = _;
+// eslint-disable-next-line no-underscore-dangle
+Vue.prototype.$_ = _;
 
 export default {
   name: 'App',
--- src/page/making-radviz.vue
+++ src/page/making-radviz.vue
@@ -4,21 +4,46 @@
       g // 테두리 원
         circle(v-bind:cx="svgWidth/2" v-bind:cy="svgHeight/2"
           v-bind:r="radvizRadius" stroke="green" v-bind:stroke-width="2" fill="rgba(0, 0, 0, 0)")
-      g // dimensions text
+      g // 테두리 원의 dimensions text
         text(v-for="(dimension, i) in dimensions"
-          :x="center.x + radvizRadius * Math.cos(getDimensionRadius(i))"
-          :y="center.y + radvizRadius * Math.sin(getDimensionRadius(i))") {{dimension}}
-
+          :x="center.x + radvizRadius * Math.cos(getDimensionTheta(i))"
+          :y="center.y + radvizRadius * Math.sin(getDimensionTheta(i))") {{dimension}}
+      g // 노드들
+        circle(v-for="(nodeData, nodeId) in nodesData"
+          :cx="getNodePosition(nodeData).x" :cy="getNodePosition(nodeData).y"
+          r="10" fill="black")
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'making-radviz',
   data() {
     return {
       svgWidth: 800,
       svgHeight: 800,
-      dimensions: ['variable a', 'variable b', 'variable c', 'variable d']
+      dimensions: ['variableA', 'variableB', 'variableC', 'variableD'],
+      nodesData: {
+        id1: {
+          variableA: 1,
+          variableB: 2,
+          variableC: 3,
+          variableD: 4
+        },
+        id2: {
+          variableA: 1,
+          variableB: 0,
+          variableC: 0,
+          variableD: 0
+        },
+        id3: {
+          variableA: 0,
+          variableB: 0,
+          variableC: 2,
+          variableD: 4
+        }
+      }
     };
   },
   computed: {
@@ -30,12 +55,34 @@ export default {
     }
   },
   methods: {
-    getDimensionRadius(index) {
+    /**
+     * @description dimensions의 index를 통해 각 dimension의 각도를 구한다.
+     * @param {number} index dimensions의 index
+     * @returns {number} 세타 각도
+     */
+    getDimensionTheta(index) {
       return ((2 * Math.PI) / this.dimensions.length) * index;
+    },
+    getNodePosition(nodeData) {
+      const sumOfaij = _.reduce(nodeData, (sum, value) => sum + value);
+      const sumOfaijMulCos = _.reduce(nodeData, (sum, value, key) => {
+        // console.log('value: ', value);
+        // console.log('key: ', key);
+        return sum + (value
+          * Math.cos(this.getDimensionTheta(_.indexOf(this.dimensions, key))));
+      }, 0);
+      const x = this.center.x + ((sumOfaijMulCos / sumOfaij) * this.radvizRadius);
+
+      const sumOfaijMulSin = _.reduce(nodeData, (sum, value, key) => sum + (value
+          * Math.sin(this.getDimensionTheta(_.indexOf(this.dimensions, key)))), 0);
+      const y = this.center.y + ((sumOfaijMulSin / sumOfaij) * this.radvizRadius);
+      console.log('cos', sumOfaijMulCos, ' sin: ', sumOfaijMulSin);
+      // console.log('x: ', x, 'y: ', y);
+      return { x, y };
     }
   },
   mounted() {
-    console.log(this.getDimensionRadius(4));
+    // this.getNodePosition(this.nodesData.id2);
   }
 };
 </script>
